a419816f222ec68051dead2286940f664d8583d9
Amanda Maria <amanda.marry@hotmail.com>
2018-12-14 10:24:20 -0300

    Adicionado jogo do rodolfo
    Adicionado jogo do rodolfo

---
+++ KAM/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1544793547,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bt_single-sheet0.png",
+		"images/bt_multiplayer-sheet0.png",
+		"images/bt_options-sheet0.png",
+		"images/relogio-sheet0.png",
+		"images/relogio-sheet1.png",
+		"images/relogio-sheet2.png",
+		"images/bt_stop-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
